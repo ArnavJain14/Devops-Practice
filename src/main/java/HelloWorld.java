c2733f7f87f11e46b7a562ff59aa17d0cb8b4b34
Arnav Jain <arnav.jain@yatra.com>
2023-09-19 22:48:30 +0530

    Second Commit Of the Day

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     //Hi Everyone This Just a Devops Project For Practice All Devops commands
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This Is DevOps Practice Project");
     }
 }
