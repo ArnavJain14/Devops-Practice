6fd474fc8ac41bac436360f8451626aa68246c48
Arnav Jain <arnav.jain@yatra.com>
2023-09-20 22:23:34 +0530

    Practice For Different Branch

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This Is DevOps Practice Project");
+        System.out.println("For Devops Commands For Git and Docker");
     }
 }
